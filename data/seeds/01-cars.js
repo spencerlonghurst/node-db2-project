b5ae8818655f4fd968e42316eab1c45f5330b87e
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-19 22:38:20 -0600

    built out my seed 01

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,41 @@
 // STRETCH
+const cars = [
+  {
+    vin: '11111111111111111',
+    make: 'Dodge',
+    model: 'Ram',
+    mileage: 128000,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+    vin: '11111111111111111',
+    make: 'Volkswagon',
+    model: 'Jetta',
+    mileage: 30000,
+    title: 'clean',
+  },
+  {
+    vin: '11111111111111111',
+    make: 'Ford',
+    model: 'Bronco',
+    mileage: 200000,
+  }
+]
+
+
+// Use either this:
+
+// exports.seed = function(knex) {
+//   return knex('cars')
+//     .truncate()(() => {
+//       return knex('cars').insert(cars)
+//     })
+// }
+
+// Or you can use the next es7 version with async/await:
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
